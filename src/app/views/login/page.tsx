475cec25b10e01d58c3da8d0c45590fc8d381d18
LoyalBrother0 <robertoserranomena@gmail.com>
2025-11-25 13:27:12 +0100

    [ADD] Botón de login con supabase (front) y componente reutilizable de redirecciones

--- src/app/views/login/page.tsx
+++ src/app/views/login/page.tsx
@@ -1,48 +1,41 @@
 'use client';
 import React from 'react';
-import './styles/login-layout.css';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import '../../styles/login-layout.css';
+import { navigation } from '@/components/redirecting';
 
 const Login: React.FC = () => {
+    //BORRAR LA SIGUIENTE FUNCIÓN CUANDO SE IMPLEMENTE EL LOGIN: - implementada V0.1
+    const { navigateTo } = navigation();
+    const formRedirect = () => {
+        navigateTo('/context-form');
+    };
     return (
-        <GoogleOAuthProvider clientId="user">
-            <div className='flex min-h-screen w-full'>
-                <div className='w-1/2 bg-[#ededed] p-8 flex flex-col justify-between'>
-                    <div>
-                        <Logo />
-                    </div>
-                    {
-                    //<Screenshot />
-                    }
+        <div className='flex min-h-screen w-full'>
+            <div className='w-1/2 bg-[#ededed] p-8 flex flex-col justify-between'>
+                <div>
+                    <Logo />
                 </div>
-                <div className='w-1/2 bg-white flex flex-col justify-center items-center px-16'>
-                    
-                    <div className="max-w-md text-center">
-                        <h2 className="text-4xl font-bold text-slate-900 mb-4">
-                            Welcome to Gada
-                        </h2>
-                        <p className="text-gray-500 text-lg">
-                            Login with your Google account
-                        </p>
-                        <div className="flex justify-center w-full mt-5">
-                            <GoogleLogin
-                                onSuccess={credentialResponse => {
-                                    console.log(credentialResponse);
-                                }}
-                                onError={() => {
-                                    console.log('Login Failed.');
-                                }}
-                                theme="outline"
-                                size="large"
-                                text="signin_with"
-                                shape="rectangular"
-                                width="350"
-                            />
-                        </div>
+                {
+                //<Screenshot />
+                }
+            </div>
+            <div className='w-1/2 bg-white flex flex-col justify-center items-center px-16'>
+                
+                <div className="max-w-md text-center">
+                    <h2 className="text-4xl font-bold text-slate-900 mb-4">
+                        Welcome to Gada
+                    </h2>
+                    <p className="text-gray-500 text-lg">
+                        Login with your Google account
+                    </p>
+                    <div className="flex justify-center w-full mt-5">
+                        <button className="btn-supabase" onClick={formRedirect}>
+                            Iniciar sesión con Supabase    
+                        </button>
                     </div>
                 </div>
             </div>
-        </GoogleOAuthProvider>
+        </div>
     );
 }
 
