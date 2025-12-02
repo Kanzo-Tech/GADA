59e2ff2b0ec3de8b1b24f67ef219e77e3db05e28
Roberto <roberto.serrano@kanzo.tech>
2025-12-02 13:10:56 +0100

    [UPD] Generación de documentos

--- src/app/api/document-generation/route.ts
+++ src/app/api/document-generation/route.ts
@@ -22,89 +22,107 @@ function expressionParser(tag: string) {
     };
 }
 
-export async function POST(request: Request){
+export async function POST(request: Request, documentToBeGenerated: string){
     try {
         const data = await request.json();
-
-        const templateRouteContract = path.join(process.cwd(), 'templates', 'Contrato_Adhesion_Institucional_v1.docx');
-        const templateRouteRulebook = path.join(process.cwd(), 'templates', 'Template_Rulebook_v1.docx');
-        const templateRouteTerms = path.join(process.cwd(), 'templates', 'Terminos_y_Condiciones_Template_v1.docx');
         const tempFolder = path.join(process.cwd(), 'temp');
+        const versionValue = data.version;
 
-        const contractContent = fs.readFileSync(templateRouteContract, 'binary');
-        const rulebookContent = fs.readFileSync(templateRouteRulebook, 'binary');
-        const termsContent = fs.readFileSync(templateRouteTerms, 'binary');
-
-        const contractZip = new PizZip(contractContent);
-        const rulebookZip = new PizZip(rulebookContent);
-        const termsZip = new PizZip(termsContent);
-
-        const contractDoc = new Docxtemplater(contractZip, {
-            paragraphLoop: true,
-            linebreaks: true,
-            parser: expressionParser,
-            delimiters: { start: '[[', end: ']]'},
-            nullGetter: function(field) {
-                return "___";
+        if (documentToBeGenerated === "dataspace-rb"){
+            const templateRouteRulebook = path.join(process.cwd(), 'templates', 'Template_Rulebook_v1.docx');
+            const rulebookContent = fs.readFileSync(templateRouteRulebook, 'binary');
+            const rulebookZip = new PizZip(rulebookContent);
+            const rulebookDoc = new Docxtemplater(rulebookZip, {
+                paragraphLoop: true,
+                linebreaks: true,
+                parser: expressionParser,
+                delimiters: { start: '[[', end: ']]'},
+                nullGetter: function(field) {
+                    return "___";
+                }
+            });
+            try{
+                await rulebookDoc.renderAsync(data);
+            } catch (error) {
+                console.error("Error while generating the docs: ", error);
+                return NextResponse.json({ error: 'Error while generating the documents'}, {status: 500});
             }
-        });
-        const rulebookDoc = new Docxtemplater(rulebookZip, {
-            paragraphLoop: true,
-            linebreaks: true,
-            parser: expressionParser,
-            delimiters: { start: '[[', end: ']]'},
-            nullGetter: function(field) {
-                return "___";
-            }
-        });
-        const termsDoc = new Docxtemplater(termsZip, {
-            paragraphLoop: true,
-            linebreaks: true,
-            parser: expressionParser,
-            delimiters: { start: '[[', end: ']]'},
-            nullGetter: function(field) {
-                return "___";
-            }
-        });
 
-        try{
-            console.log(data);
-            await contractDoc.renderAsync(data);
-            await rulebookDoc.renderAsync(data);
-            await termsDoc.renderAsync(data);
-        } catch (error) {
-            console.error("Error while generating the docs: ", error);
-            return NextResponse.json({ error: 'Error while generating the documents'}, {status: 500});
+            const wordRulebookGenerated = rulebookDoc.getZip().generate({
+                type: 'nodebuffer',
+                compression: 'DEFLATE',
+            });
+
+            const nameRulebookWord = 'Rulebook_' + versionValue + '.docx';
+            const storageRouteRulebook = path.join(tempFolder, nameRulebookWord);
+            fs.writeFileSync(storageRouteRulebook, wordRulebookGenerated);
+            return NextResponse.json({status: 200})
         }
 
-        const wordContractGenerated = contractDoc.getZip().generate({
-            type: 'nodebuffer',
-            compression: 'DEFLATE',
-        });
-        const wordRulebookGenerated = rulebookDoc.getZip().generate({
-            type: 'nodebuffer',
-            compression: 'DEFLATE',
-        });
-        const wordTermsGenerated = termsDoc.getZip().generate({
-            type: 'nodebuffer',
-            compression: 'DEFLATE',
-        });
+        if (documentToBeGenerated === "membership-agreement"){
+            const templateRouteContract = path.join(process.cwd(), 'templates', 'Contrato_Adhesion_Institucional_v1.docx');
+            const contractContent = fs.readFileSync(templateRouteContract, 'binary');
+            const contractZip = new PizZip(contractContent);
+            const contractDoc = new Docxtemplater(contractZip, {
+                paragraphLoop: true,
+                linebreaks: true,
+                parser: expressionParser,
+                delimiters: { start: '[[', end: ']]'},
+                nullGetter: function(field) {
+                    return "___";
+                }
+            });
+            try{
+                await contractDoc.renderAsync(data);
+            } catch(error) {
+                console.error("Error while generating the docs: ", error);
+                return NextResponse.json({ error: 'Error while generating the documents'}, {status: 500});
+            }
+            
+            const wordContractGenerated = contractDoc.getZip().generate({
+                type: 'nodebuffer',
+                compression: 'DEFLATE',
+            });
 
-        const versionValue = data.version;
-        const nameContractWord = 'Contrato_Adhesion_Institucional_' + versionValue + '.docx';
-        const nameRulebookWord = 'Rulebook_' + versionValue + '.docx';
-        const nameTermsWord = 'Terminos_y_Condiciones_' + versionValue + '.docx';
-
-        const storageRouteContract = path.join(tempFolder, nameContractWord);
-        const storageRouteRulebook = path.join(tempFolder, nameRulebookWord);
-        const storageRouteTerms = path.join(tempFolder, nameTermsWord);
-
-        fs.writeFileSync(storageRouteContract, wordContractGenerated);
-        fs.writeFileSync(storageRouteRulebook, wordRulebookGenerated);
-        fs.writeFileSync(storageRouteTerms, wordTermsGenerated);
-        return NextResponse.json({status: 200})
+            const nameContractWord = 'Contrato_Adhesion_Institucional_' + versionValue + '.docx';
+            const storageRouteContract = path.join(tempFolder, nameContractWord);
+            fs.writeFileSync(storageRouteContract, wordContractGenerated);
+            return NextResponse.json({status: 200})
+        }
+        
+        if (documentToBeGenerated === "general-tc"){
+            const templateRouteTerms = path.join(process.cwd(), 'templates', 'Terminos_y_Condiciones_Template_v1.docx');
+            const termsContent = fs.readFileSync(templateRouteTerms, 'binary');
+            const termsZip = new PizZip(termsContent);
+            const termsDoc = new Docxtemplater(termsZip, {
+                paragraphLoop: true,
+                linebreaks: true,
+                parser: expressionParser,
+                delimiters: { start: '[[', end: ']]'},
+                nullGetter: function(field) {
+                    return "___";
+                }
+            });
+            try{
+                await termsDoc.renderAsync(data);
+            } catch(error) {
+                console.error("Error while generating the docs: ", error);
+                return NextResponse.json({ error: 'Error while generating the documents'}, {status: 500});
+            }
+            
+            const wordTermsGenerated = termsDoc.getZip().generate({
+                type: 'nodebuffer',
+                compression: 'DEFLATE',
+            });
+            const nameTermsWord = 'Terminos_y_Condiciones_' + versionValue + '.docx';
+            const storageRouteTerms = path.join(tempFolder, nameTermsWord);
+            fs.writeFileSync(storageRouteTerms, wordTermsGenerated);
+            return NextResponse.json({status: 200})
+        }
+        
+        return NextResponse.json({ error: 'Something went wrong.'}, { status: 50 });
     } catch (e) {
         console.error(e);
-        return NextResponse.json({ error: 'Something went wrong' }, { status: 50 });
+        return NextResponse.json({ error: 'Something went wrong.' }, { status: 50 });
     }
 }
